58fb5c8e5f4f9f51483e5b9cdd2691d43f6a0bc3
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-05 22:08:29 +0000

    script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,6 @@
  */
 int main(void)
 {
-	char my_char=0;
-	int my_int=0;
-	long int my_long_int=0;
-	long long int my_long_long_int=0;
-	float my_float=0;
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
