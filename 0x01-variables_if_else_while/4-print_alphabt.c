9b89a7960deacc616d8040d69af0e4fd9f7af35b
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-06 20:49:12 +0000

    script 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: always 0
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' && l != 'e')
+		{
+			putchar(l);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
