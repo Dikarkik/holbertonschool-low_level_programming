8a3eb64e2fb66a40ddd04741f6d333daa8cf1403
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-06 21:30:42 +0000

    script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
