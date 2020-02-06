a0594dfe544c6341954cbd153913add2c4c97258
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-06 20:33:46 +0000

    script 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: always 0
+ */
+
+int main(void)
+{
+	char l;
+	char al;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (al = 'A'; al <= 'Z'; al++)
+        {
+                putchar(al);
+        }
+	putchar('\n');
+
+	return (0);
+}
