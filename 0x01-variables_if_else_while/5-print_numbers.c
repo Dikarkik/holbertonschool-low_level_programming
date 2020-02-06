5aed65971cc499c25019b679a23a04322483ee05
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-06 21:01:05 +0000

    script 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
