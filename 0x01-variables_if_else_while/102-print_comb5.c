4cbeaedc548880efa300c1a6dcf14b3c8f79f5f9
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-07 04:33:15 +0000

    script 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int x;
+	int j;
+	int u;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (x = 48; x <= 56; x++)
+		{
+			for (j = i; j <= 57; j++)
+			{
+				for (u = x + 1; u <= 57; u++)
+				{
+					putchar(i);
+					putchar(x);
+					putchar(32);
+					putchar(j);
+					putchar(u);
+
+					if (!(i == 57 && x == 56 && j == 57 && u == 57))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+
+			}
+
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
