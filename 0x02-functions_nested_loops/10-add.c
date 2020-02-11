cdc39f7acc3a72dfb24afd51e96d769b4fe010ba
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-11 19:54:00 +0000

    script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - Return the sum of two integers.
+ *@a: firts integer
+ *@b: second integer
+ * Return: the result of a + b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
