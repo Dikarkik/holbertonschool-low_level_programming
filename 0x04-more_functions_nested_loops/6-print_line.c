f1aefa54bdbef09118c5099d5cc088a520e27d77
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-13 20:15:29 +0000

    script 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
