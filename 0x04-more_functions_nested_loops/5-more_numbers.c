bd25dedd00e8128d4fa92cc3b96c1997ec7eb845
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-13 20:04:10 +0000

    script 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+			_putchar((i / 10) + '0');
+
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
