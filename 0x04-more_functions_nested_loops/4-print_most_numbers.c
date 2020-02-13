6815cb0473be8e8a6438f9cd54cc1710e7190a55
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-13 19:26:14 +0000

    scripts 2, 3, 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line, except the 2 and 4.
+ *
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,7 +2,11 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* HOLBERTON_H */
