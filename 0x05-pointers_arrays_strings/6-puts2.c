59bf4f706a718560550e86bb97a3b19e1aef1d0b
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-18 20:18:57 +0000

    fix script 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts2 - prints every other character of a string, starting with the first.
+ * puts2 - prints every other character of a string, starting with the first.
  *@str: the string.
  * Return: void.
  */
@@ -9,9 +9,9 @@ void puts2(char *str)
 {
 	int count;
 
-	while(str[count] != '\0')
+	while (str[count] != '\0')
 	{
-		if(count % 2 == 0)
+		if (count % 2 == 0)
 			_putchar(str[count]);
 		count++;
 	}
