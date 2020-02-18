cb7921a7081744c2bcfe964b47d3af52ffcb72c5
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-18 23:03:34 +0000

    fix script 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,21 +9,13 @@
  */
 void print_array(int *a, int n)
 {
-	int size = 0;
 	int i = 0;
 
-	while (a[size])
-		size++;
-
-	if (n <= size && n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i < (n - 1))
-				printf("%d, ", a[i]);
-			else
-				printf("%d\n", a[i]);
-		}
-
+		if (i < (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
 	}
 }
