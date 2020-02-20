113ecc915bb47995556a436038018e8ad037dd9b
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-20 15:22:35 +0000

    script 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strcat - concatenates two strings.
+ *@dest: string.
+ *@src: string to concatenate in dest.
+ *Return: a pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+	int count2 = 0;
+
+	while (dest[count])
+	{
+		count++;
+	}
+
+	while (src[count2])
+	{
+		dest[count] = src[count2];
+		count++;
+		count2++;
+	}
+
+	return (dest);
+}
