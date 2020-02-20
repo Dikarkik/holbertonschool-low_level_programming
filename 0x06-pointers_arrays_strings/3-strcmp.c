2501dd0b5ad92c5f8aeceaf436d8ed2b841e77a0
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-20 19:38:33 +0000

    script 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compares two strings.
+ *@s1: string 1.
+ *@s2: string 2.
+ *Return: 0 if s1 == s2. 1 if s1 > s2. -1 if s1 < s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (0);
+	else if (s1 > s2)
+		return (1);
+	else
+		return (-1);
+}
