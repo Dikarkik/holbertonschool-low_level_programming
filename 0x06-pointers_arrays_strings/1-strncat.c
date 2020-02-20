dd9da6394d4c5d7b68c64b1b6cf05e9a00329bea
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-20 18:46:17 +0000

    fix script 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  *_strncat - concatenates two strings (use at most n bytes from src).
  *@dest: string.
  *@src: string to concatenate in dest.
- *@n: use max n bytes of src.
+ *@n: use at most n bytes of src.
  *Return: a pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
@@ -21,6 +21,8 @@ char *_strncat(char *dest, char *src, int n)
 		count++;
 		count2++;
 	}
+
+	dest[count] = '\0';
 
 	return(dest);
 }
