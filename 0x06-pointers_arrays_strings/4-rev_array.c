5efcf8a7bf51378c49275a8522de2ec17ac8241b
DianaQ <dianacarolinaquinterocaro@gmail.com>
2020-02-20 20:29:52 +0000

    script 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *reverse_array - reverses the content of an array of integers.
+ *@a: the array.
+ *@n: is the number of elements of the array.
+ *Return: void.
+ */
+void reverse_array(int *a, int n)
+{
+	int middle = n / 2;
+	int count = 0;
+	int other = n - 1;
+	int backup;
+
+	for (count = 0; count <= middle; count++)
+	{
+		backup = a[count];
+		a[count] = a[other];
+		a[other] = backup;
+		other--;
+	}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,10 +2,12 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+void print_array(int *a, int n);
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif /* HOLBERTON_H */
