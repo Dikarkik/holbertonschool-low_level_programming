2e6379dc379abf8ea01598b46f6533b86c2842c2
KaroDev3 <karodev3@gmail.com>
2020-02-24 19:49:53 +0000

    script 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring.
+ *@haystack: string.
+ *@needle: sub string.
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int checked = 0;
+	int size_n = 0;
+
+	while (needle[size_n])
+		size_n++;
+
+	size_n--;
+
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[checked])
+		{
+			if (checked == size_n)
+				return (&haystack[i - checked]);
+
+			checked++;
+		}
+		else
+			checked = 0;
+
+		i++;
+	}
+
+	return ('\0');
+}
