ee84f788dfa496a476a2454381b502de9e8921b2
KaroDev3 <karodev3@gmail.com>
2020-02-26 21:20:15 +0000

    fix script 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (!*s)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 
 	_putchar(*s);
-
-	if (s++)
-		_puts_recursion(s++);
-
+      	_puts_recursion(++s);
 }
