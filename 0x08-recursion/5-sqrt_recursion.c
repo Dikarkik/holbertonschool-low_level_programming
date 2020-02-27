367d7c649e50549fa1a2099db52d03d1a91dcc5d
KaroDev3 <karodev3@gmail.com>
2020-02-27 12:39:43 +0000

    script 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,35 @@
 #include "holberton.h"
+int check_posible_root(int n, int root);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number.
- *@n: radicando?
+ *@n: radicand.
  * Return: the natural square root of a n.
+ * If n does not have a natural square root,
+ * the function should return -1
  */
 int _sqrt_recursion(int n)
 {
+	if (n <= 0)
+		return (-1);
 
+	return (check_posible_root(n, 1));
+}
+
+/**
+ * check_posible_root - check square root for n.
+ *@n: radicand.
+ *@root: posible root.
+ * Return: root of n, -1 if n does not have a natural square root.
+ */
+int check_posible_root(int n, int root)
+{
+	int result = root * root;
+
+	if (result == n)
+		return (root);
+	else if (result < n)
+		return (check_posible_root(n, root + 1));
+	else
+		return (-1);
 }
