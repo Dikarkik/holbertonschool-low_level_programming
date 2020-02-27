ec4b97955ce1bff3db9f03d7784c8cb6622392ff
KaroDev3 <karodev3@gmail.com>
2020-02-27 11:41:18 +0000

    fix script 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,29 +8,28 @@ int check_each_div(int n, int div);
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n <= 0)
 		return (0);
+
+	else if (check_each_div(n, 1) == 2)
+		return (1);
 	else
-		return (check_each_div(n, 2));
+		return (0);
 }
 
 /**
- * check_each_div - check for prime number.
- *@n: number to check.
- *@div: posible divisor, i use "modulo" to check that.
- * Return: 1 if n is prime, 0 otherwise.
+ * check_each_div - check each posible divisor of a number.
+ *@n: number.
+ *@div: posible divisor, should start in 1.
+ * Return: count of times when n % div is == 0.
  */
 int check_each_div(int n, int div)
 {
 	if (div == n)
 		return (1);
 
-	if (n % div == 0)
-	{
-		check_each_div(n, (div + 1));
-	}
+	else if (n % div == 0)
+		return (1 + check_each_div(n, (div + 1)));
 	else
-		return (0);
-
-	return (3);
+		return (0 + check_each_div(n, (div + 1)));
 }
