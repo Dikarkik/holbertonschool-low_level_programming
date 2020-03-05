dbbf7b2da5b1b7a16f489028bcb99f6cfbeef7f9
KaroDev3 <karodev3@gmail.com>
2020-03-05 18:29:15 +0000

    fix script 3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array of nmemb elements of
+ * _calloc - allocates memory for an array of
+ * nmemb elements of size bytes each.
+ * The memory is set to zero.
  * @nmemb: number of elements.
  * @size: size of every element.
- * size bytes each and returns a pointer to the allocated memory.
- * The memory is set to zero.
- * Return: if nmemb or size is 0, then returns NULL.
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, then returns NULL.
  * If malloc fails, then returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
 	p = malloc(nmemb * size);
 
