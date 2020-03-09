698b8320ecfac33f3343249d8b998e794166f82a
KaroDev3 <karodev3@gmail.com>
2020-03-09 23:51:15 +0000

    script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,8 +30,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog->age = age;
 
 	if (!my_dog || !my_dog->name || !my_dog->owner)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		free(my_dog->owner);
 		return (NULL);
-
+	}
 	index = 0;
 	while (name[index])
 	{
