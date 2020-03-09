b6afa67479bcaca40e87bdd7949a0725c7fce0a7
KaroDev3 <karodev3@gmail.com>
2020-03-09 20:24:31 +0000

    script 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog.
+ * @name: name.
+ * @age: age.
+ * @owner: owner.
+ * Description: Dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG */
