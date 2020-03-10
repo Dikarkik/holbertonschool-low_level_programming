ef069af1edb7a32a4152c76519d10a547249cb27
KaroDev3 <karodev3@gmail.com>
2020-03-10 03:29:01 +0000

    fix script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,40 @@
 #include "dog.h"
 
 /**
+ * len - return the length of an stream.
+ * @str: string.
+ * Return: the length of an stream.
+ */
+int len(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * str_copy - copy str to dest.
+ * @dest: destination.
+ * @str: string.
+ * Return: void.
+ */
+void str_copy(char *dest, char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+}
+
+/**
  * new_dog - creates a new dog.
  * @name: name.
  * @age: age.
@@ -13,19 +47,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *my_dog;
 	int len_name;
 	int len_owner;
-	int index;
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
 
-	len_name = 0;
-	while (name[len_name])
-		len_name++;
-
-	len_owner = 0;
-	while (owner[len_owner])
-		len_owner++;
+	len_name = len(name);
+	len_owner = len(owner);
 
 	my_dog->name = malloc(sizeof(char) * len_name + 1);
 	if (!my_dog->name)
@@ -44,21 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	my_dog->age = age;
 
-	index = 0;
-	while (name[index])
-	{
-		my_dog->name[index] = name[index];
-		index++;
-	}
-	my_dog->name[index] = '\0';
-
-	index = 0;
-	while (owner[index])
-	{
-		my_dog->owner[index] = owner[index];
-		index++;
-	}
-	my_dog->owner[index] = '\0';
+	str_copy((*my_dog).name, name);
 
 	return (my_dog);
 }
