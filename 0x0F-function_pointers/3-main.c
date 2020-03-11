f74479fa7e57a2aafe0603188aa779fd71c354de
KaroDev3 <karodev3@gmail.com>
2020-03-11 21:53:23 +0000

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * main - this program performs simple operations and print the result.
+ * usage is calc num1 operator num2.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: 0 on success. 98, 99, 100 on error.
+ */
+int main(int argc, char *argv)
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+
+	return (0);
+}
