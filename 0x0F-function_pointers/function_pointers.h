df321a5978be344086a3a24026b78064de79640f
KaroDev3 <karodev3@gmail.com>
2020-03-11 19:05:42 +0000

    script 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -5,5 +7,6 @@ int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION POINTERS_H */
