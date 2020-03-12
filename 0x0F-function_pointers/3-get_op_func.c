07df4cb5e611fadffe7c83fca0243ab1744a19e3
KaroDev3 <karodev3@gmail.com>
2020-03-12 21:40:48 +0000

    fix task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,15 +20,19 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	if (s[1] == '\0')
+	if (s[1] != '\0')
 	{
-
-		while (ops[i].op)
-		{
-			if (ops[i].op[0] == s[0])
-				return (ops[i].f);
-			i++;
-		}
+		printf("Error\n");
+		exit(99);
 	}
+
+
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+
 	return (NULL);
 }
