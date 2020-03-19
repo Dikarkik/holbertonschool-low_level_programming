56e54f723e564057cd7d06dc033be4a4fd1fcc8f
KaroDev3 <karodev3@gmail.com>
2020-03-19 19:13:38 +0000

    fix task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (!h->str || h->str[0] == '\0')
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
