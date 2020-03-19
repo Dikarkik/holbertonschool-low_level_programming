4326411c0b700c140d601dd2c756cac2a8d023fa
KaroDev3 <karodev3@gmail.com>
2020-03-19 18:55:17 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * If str is NULL, print [0] (nil).
+ * @h: pointer to list_t element.
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int index = 0;
+
+	while (h != NULL)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+
+		printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		index++;
+	}
+
+	return (index);
+}
