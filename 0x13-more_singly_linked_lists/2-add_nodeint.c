392291e26d391ef573b70a02845ed18ef6f4965d
KaroDev3 <karodev3@gmail.com>
2020-03-23 15:38:35 +0000

    fix task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,6 +3,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: pointer to the head of the listint_t list.
+ * @n: number to store in the new node.
  * Return: the address of the new element, or NULL if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
