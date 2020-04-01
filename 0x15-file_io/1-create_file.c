4b39ee0e766353b9899acc4472a5078b2a02fa8f
KaroDev3 <karodev3@gmail.com>
2020-04-01 03:14:49 +0000

    fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (_file == -1)
 		return (-1);
 
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	_write = write(_file, text_content, len);
 	if (_write == -1)
